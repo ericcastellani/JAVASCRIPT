f15a423b6451cbec4583eb1430c594cd32b654f8
eric <ecastellani@outlook.fr>
2022-12-07 11:07:24 +0100

    intro javascript switch case et boucles

--- mon_script.js
+++ mon_script.js
@@ -35,3 +35,43 @@ var texte= "Le prix est : "//On définit une phrase quiapparaîtraen console
 var resultat=(estMembre  ?  "15  €"  :  "30  €")//On  teste  si  estMembre  est  vrai  ou  faux,  puis  on  stockela réponse dans la variable "résultat". "estMembre" étant faux, la valeur stockée sera "30 €".
 console.log(texte+resultat); //On affiche le résultat
 
+    //Switch case
+
+var estCoupable = "innocent";
+switch(estCoupable){
+    case "coupable":
+        console.log("Vous  allez  sur  la  case  prison,  ne  passez  pas  par  la case  départ,  ne  touchez  pas 20000 €");
+        break;
+        case"innocent":
+        console.log("Tirez une carte chance!");
+        break;
+        default:
+        console.log("Vous échappez à la justice.");
+        break;
+}
+    //Boucles for - do while - while
+        //for
+var table = 7; //On cherchera à afficher la table de 7
+for ( var i = 0; i < 11; i++ ) {//On définit un compteur, une limite afin de pouvoir stopper la boucle et enfin une incrémentation
+    console.log(i+' fois '+table+' est égale à '+ i*table);//On affiche dans la console lesvariable, le calcul et du texte
+            //Le symbole «+» sert ici à la concaténation
+}
+
+        //do while
+
+var compteur = 10; // On définit un compteur
+do { //On indique laliste d’instructions
+    console.log('Il  reste  '+compteur+'  étapes  avant  la  fin  du  compte  à  rebours') //Ici,  on  lui  demande d’afficher la variable «compteur» accompagnée de texte
+        compteur --; // On décrémente «compteur»
+        } while(compteur > 0); // La boucle continuera jusqu’à ce que «compteur» arrive à 0
+        
+        //while
+
+var nbRepas = 0, repas = "biscuit"; //On définit les variables
+    while(nbRepas < 13) { //Tant que «nbRepas» est plus petit que 13
+        if (nbRepas > 1) { //Le but ici est de définit si «biscuit» prend un «s» ou pas, en fonction du nombre présent
+            repas = "biscuits";}console.log('Vous avez mangé '+nbRepas+' '+repas);//On affiche le résultat
+            nbRepas++; //On incrémente lavariable
+        }
+
+        //Les avénements
